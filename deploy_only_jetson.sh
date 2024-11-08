edb0e4cae5bef6ed9440c3370c9ed9c267ad8e33
Alexander Hoffman <alxhoff@gmail.com>
2024-11-08 09:51:38 +0100

    Simple deploy only script

---
+++ deploy_only_jetson.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Simple script to deploy a compiled kernel to a Jetson device
+# Usage: ./deploy_only_jetson.sh <device-ip>
+# Arguments:
+#   <device-ip>  The IP address of the target Jetson device
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <device-ip>"
+  exit 1
+fi
+
+DEVICE_IP=$1
+
+# Deploy to Jetson device
+python3 kernel_deployer.py deploy-jetson --kernel-name jetson --ip $DEVICE_IP --user cartken
