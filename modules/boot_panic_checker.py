06e5526d72c4b7e212dd848bb7a1a1ee893dd4c1
Alexander Hoffman <alxhoff@gmail.com>
2024-11-25 15:04:13 +0100

    Untested VIKI loggin boot script

--- modules/boot_panic_checker.py
+++ modules/boot_panic_checker.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 import os
+import viki
+
+VIKI = viki.get_logger(__name__)
+panic_log_path = "/var/log/panic.log"
 
 def viki_code():
-    print("Running VIKI code...")
+    try:
+        with open(panic_log_path) as f:
+            log = f.read()
+            VIKI.error("KERNEL_PANIC_LOGGER_PANIC_DETECTED", log)
+    except Exception as e:
+        print(f"Failed to open log: {}", e)
 
 def check_and_handle_panic_log():
-    panic_log_path = "/var/log/panic.log"
     if os.path.exists(panic_log_path):
         viki_code()
         os.remove(panic_log_path)
