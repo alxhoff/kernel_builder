053f013247a6362da5c868c1cbf4ad8caf6a0e43
Alexander Hoffman <alxhoff@gmail.com>
2025-05-30 17:30:34 +0200

    Added getting pinmux to tegra setup

--- scripts/rootfs/setup_tegra_package.sh
+++ scripts/rootfs/setup_tegra_package.sh
@@ -282,7 +282,11 @@ chmod +x "$TEGRA_DIR/"*.sh
 echo "All rootfs scripts downloaded successfully."
 
 cd $TEGRA_DIR
+echo "Setting up rootfs with nvidia binaries and default user"
 sudo $TEGRA_DIR/setup_rootfs.sh --l4t-dir $TEGRA_DIR
+
+echo "Getting pinmux files"
+such $TEGRA_DIR/get_pinmux.sh --l4r-dir $TEGRA_DIR
 
 if [[ "$JUST_CLONE" == true ]]; then
 	exit 1
