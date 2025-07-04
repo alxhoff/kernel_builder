546fd3f4eeb29ad5e45b40b4c59288e31f68a98e
Alexander Hoffman <alxhoff@gmail.com>
2025-07-04 08:59:46 +0200

    Build 6.2 now uses absolute paths

--- scripts/kernel_builder/build_6_2.sh
+++ scripts/kernel_builder/build_6_2.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-./scripts/kernel_builder/compile_and_package.sh cartken_6_2 --localversion cartken6.2 --dtb-name tegra234-p3737-0000+p3701-0000.dtb
+SCRIPT_DIR="$(realpath "$(dirname "$0")/..")"
+cd $SCRIPT_DIR/..
+$SCRIPT_DIR/kernel_builder/compile_and_package.sh cartken_6_2 --localversion cartken6.2 --dtb-name tegra234-p3737-0000+p3701-0000.dtb
